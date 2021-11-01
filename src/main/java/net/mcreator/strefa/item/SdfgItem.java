bc621ec9cf9a9998548b1174eb0692541bb4906a
ocelotxd <64527512+ocelotxd@users.noreply.github.com>
2021-11-01 09:34:50 +0100

    sdfsdf

---
+++ src/main/java/net/mcreator/strefa/item/SdfgItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.strefa.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.strefa.TheocelotmodModElements;
+
+@TheocelotmodModElements.ModElement.Tag
+public class SdfgItem extends TheocelotmodModElements.ModElement {
+	@ObjectHolder("theocelotmod:sdfg")
+	public static final Item block = null;
+	public SdfgItem(TheocelotmodModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("sdfg");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
